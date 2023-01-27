be6caf16e0293ed47a24db33c23575064ef39ba3
Tushar Rayamajhi <84193854+tusharrayamajhi@users.noreply.github.com>
2023-01-27 16:58:53 +0545

    Create abc.java

---
+++ abc.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class abc {
+    public static void main(String args[]) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.Addfirst(1);
+        ll.Addlast(2);
+        System.out.println(ll);
+
+    }
+}
